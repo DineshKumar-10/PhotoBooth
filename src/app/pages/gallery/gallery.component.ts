f0b3d7bbed6cb2be52630803c78c582f278f28a8
Dinesh10 <dineshiam95@gmail.com>
2026-01-23 14:25:27 +0530

    service-gallery

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent {
+  images = [
+    'assets/gallery/Beige and Green Simple Photo.png',
+    'assets/gallery/BlogLog.jpeg',
+    'assets/gallery/cycling-bicycle.gif',
+    'assets/gallery/Hdf009efeadd34d61bd02d36ef084df1c2.jpg_720x720q50.avif',
+    'assets/gallery/OIP.webp',
+    'assets/gallery/OIP.webp'
+  ];
 
+  selectedImage: string | null = null;
+
+  open(img: string) {
+    this.selectedImage = img;
+  }
+
+  close() {
+    this.selectedImage = null;
+  }
 }
