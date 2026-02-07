42b6317225fedca3a328189627997f8d603b88e8
Dinesh10 <dineshiam95@gmail.com>
2026-02-07 20:34:44 +0530

    booking_service_update

--- src/app/services/booking.service.ts
+++ src/app/services/booking.service.ts
@@ -3,31 +3,59 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Booking } from '../models/booking';
 
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class BookingService {
+
+//   private apiUrl = 'https://booking-backend-1-id0e.onrender.com/';
+
+//   constructor(private http: HttpClient) {}
+
+//   getAllBookings(): Observable<Booking[]> {
+//     return this.http.get<Booking[]>(this.apiUrl);
+//   }
+
+//   approveBooking(id: number) {
+//   return this.http.put(
+//     `https://booking-backend-1-id0e.onrender.com//${id}/approve`,
+//     {}
+//   );
+// }
+
+// rejectBooking(id: number) {
+//   return this.http.put(
+//     `https://booking-backend-1-id0e.onrender.com//${id}/reject`,
+//     {}
+//   );
+// }
+
+// }
+
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class BookingService {
 
-  private apiUrl = 'https://booking-backend-1-id0e.onrender.com/';
+  private apiUrl = 'https://booking-backend-1-id0e.onrender.com/api/bookings';
 
   constructor(private http: HttpClient) {}
 
-  getAllBookings(): Observable<Booking[]> {
+  createBooking(data: Booking) {
+    return this.http.post(this.apiUrl, data);
+  }
+
+  getAllBookings() {
     return this.http.get<Booking[]>(this.apiUrl);
   }
 
   approveBooking(id: number) {
-  return this.http.put(
-    `https://booking-backend-1-id0e.onrender.com//${id}/approve`,
-    {}
-  );
-}
+    return this.http.put(`${this.apiUrl}/${id}/approve`, {});
+  }
 
-rejectBooking(id: number) {
-  return this.http.put(
-    `https://booking-backend-1-id0e.onrender.com//${id}/reject`,
-    {}
-  );
-}
-
+  rejectBooking(id: number) {
+    return this.http.put(`${this.apiUrl}/${id}/reject`, {});
+  }
 }
